7690ee8442f04030737f6f23136ec21a479eb78d
NIKER <niker@niker-virtual-machine.(none)>
2012-12-11 20:32:25 +0100

    Returned teh delayed CPU settings even the governor settings are required, they get tampered with by something :/

--- kernelinjector.oxp/structure.new/elitekernel_delayed.sh
+++ kernelinjector.oxp/structure.new/elitekernel_delayed.sh
@@ -1,8 +1,55 @@
 #!/system/bin/sh
 
-sleep 45 # do the configuration again to override ROM hardcoded stuff
+sleep 60 # do the configuration again to override ROM and tegra hardcoded stuff
 
-# set vm tweaks
+# need to enable all CPU cores in order to set them up
+echo 4 > /sys/power/pnpmgr/hotplug/min_on_cpus
+# this needs to be applied, wait for a bit
+sleep 3 
+
+# set governors again
+echo "ondemand" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+echo "ondemand" > /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor
+echo "ondemand" > /sys/devices/system/cpu/cpu2/cpufreq/scaling_governor
+echo "ondemand" > /sys/devices/system/cpu/cpu3/cpufreq/scaling_governor
+
+# set default speeds again (cpus activate in order 0-3-2-1)
+echo "1300000" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
+echo "1600000" > /sys/devices/system/cpu/cpu1/cpufreq/scaling_max_freq
+echo "1500000" > /sys/devices/system/cpu/cpu2/cpufreq/scaling_max_freq
+echo "1400000" > /sys/devices/system/cpu/cpu3/cpufreq/scaling_max_freq
+
+echo "51000" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
+echo "51000" > /sys/devices/system/cpu/cpu1/cpufreq/scaling_min_freq
+echo "51000" > /sys/devices/system/cpu/cpu2/cpufreq/scaling_min_freq
+echo "51000" > /sys/devices/system/cpu/cpu3/cpufreq/scaling_min_freq
+
+sync
+# reset core activation to default
+echo 0 > /sys/power/pnpmgr/hotplug/min_on_cpus
+
+# set ondemand prefs again
+echo "80" > /sys/devices/system/cpu/cpufreq/ondemand/up_threshold
+echo "15" > /sys/devices/system/cpu/cpufreq/ondemand/down_differential
+echo "1" > /sys/devices/system/cpu/cpufreq/ondemand/ignore_nice_load
+echo "3000000" > /sys/devices/system/cpu/cpufreq/ondemand/input_boost_duration
+echo "1" > /sys/devices/system/cpu/cpufreq/ondemand/io_is_busy
+echo "5" > /sys/devices/system/cpu/cpufreq/ondemand/powersave_bias
+echo "5" > /sys/devices/system/cpu/cpufreq/ondemand/sampling_down_factor
+echo "30000" > /sys/devices/system/cpu/cpufreq/ondemand/sampling_rate
+echo "10000" > /sys/devices/system/cpu/cpufreq/ondemand/sampling_rate_min
+echo "1" > /sys/devices/system/cpu/cpufreq/ondemand/touch_poke
+echo "51000" > /sys/devices/system/cpu/cpufreq/ondemand/two_phase_bottom_freq
+echo "1" > /sys/devices/system/cpu/cpufreq/ondemand/two_phase_dynamic
+echo "340000" > /sys/devices/system/cpu/cpufreq/ondemand/two_phase_freq
+echo "3" > /sys/devices/system/cpu/cpufreq/ondemand/ui_counter
+echo "20000" > /sys/devices/system/cpu/cpufreq/ondemand/ui_sampling_rate
+echo "66" > /sys/devices/system/cpu/cpufreq/ondemand/ux_boost_threshold
+echo "760000" > /sys/devices/system/cpu/cpufreq/ondemand/ux_freq
+echo "20" > /sys/devices/system/cpu/cpufreq/ondemand/ux_loading
+
+
+# set vm tweaks again
 sysctl -w vm.min_free_kbytes=5242
 sysctl -w vm.vfs_cache_pressure=30
 sysctl -w vm.swappiness=40
@@ -18,7 +65,7 @@ sysctl -w vm.overcommit_ratio=20
 sysctl -w kernel.panic_on_oops=1
 sysctl -w kernel.panic=10
 
-# minfree
+# minfree again
 echo "0,1,2,5,7,15" > /sys/module/lowmemorykiller/parameters/adj
 echo "1536,3072,6144,11264,16384,20480" > /sys/module/lowmemorykiller/parameters/minfree
 
